a7a8198f937fa495dc656803d1914194ab84fbba
Suvradippaul <suvradip2000@gmail.com>
2022-10-05 18:41:07 +0530

    Fix code formatting

--- 42. Trapping Rain Water/Solution.java
+++ 42. Trapping Rain Water/Solution.java
@@ -1,12 +1,12 @@
+// Two pointers approach
+// INTUITION - 
+// 1. At each step, update left max height and right max height.
+// 2. How much water can we get at each step? -> Min(leftMax, rightMax) - current height
+
 class Solution {
     public int trap(int[] arr) {
         return solve(arr);
     }
-    
-    // Two pointers approach
-	// INTUITION - 
-	// 1. At each step, update left max height and right max height.
-	// 2. How much water can we get at each step? -> Min(leftMax, rightMax) - current height
     
 	int solve(int[] arr) {
 		int l = 0, r = arr.length - 1, sum = 0, lMax = 0, rMax = 0;
